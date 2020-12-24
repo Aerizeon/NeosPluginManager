54ec504ab2eb4ae3c044f6cb6957874642c12b8f
Alexander Terry <alexterry43@gmail.com>
2020-12-24 00:57:09 -0900

    require lockin to press

---
+++ NeosPluginManager/Patches/PatchFriendsDialog.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using FrooxEngine;
+using FrooxEngine.UIX;
+
+namespace NeosPluginManager.Patches
+{
+    [HarmonyPatch(typeof(FriendsDialog), "AddFriendItem")]
+    public static class PatchFriendsDialog
+    {
+        /// <summary>
+        /// patch neos's friends list to not require lock into press
+        /// </summary>
+        /// 
+        static void Postfix(ref FriendItem __result)
+        {
+            Button button = __result.Slot.GetComponentInChildren<Button>();
+            button.RequireLockInToPress.Value = true;
+        }
+    }
+}
